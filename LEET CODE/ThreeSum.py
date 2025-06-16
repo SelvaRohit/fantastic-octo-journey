bbb4c419da059c299d2a226201fb8f968509dbfb
Selva <selvakaruppasamy2018@gmail.com>
2025-06-16 13:35:53 +0530

    Changed the logic to find all the combinatiion

--- LEET CODE/ThreeSum.py
+++ LEET CODE/ThreeSum.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        output=[]
         for i in range(len(nums)):
             heap={}
             target=(0-nums[i])
@@ -12,8 +13,8 @@ class Solution(object):
                 if nums[currentindex] not in heap:
                     heap[target-nums[currentindex]]=currentindex
                 else:
-                    return i,j,currentindex
-        return None,None,None #if not a any 3 numbers sum is equal to target
+                    output.append([i,heap[target-nums[currentindex]],currentindex])
+        # return None,None,None #if not a any 3 numbers sum is equal to target
 obj=Solution()
 nums=[-1,0,1,2,-1,-4]
 print(obj.threeSum(nums))
