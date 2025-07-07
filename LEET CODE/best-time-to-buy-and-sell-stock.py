430cd5894800ce90f7f37f4744e1b6ad63fa0146
Selva <selvakaruppasamy2018@gmail.com>
2025-07-07 15:59:45 +0530

    Created a  new file for the best-time-to-buy-and-sell-stock problem and started solving.

---
+++ LEET CODE/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        maximumProfit=0
+        i=0
+        j=1
+        profit_achieved_days={}
+        # for i,price in enumerate(prices):
+        while i < len(prices) and j < len(prices):
+            profit=prices[j] - prices[i]
+            if (profit) < 0:
+                i+=1
+            else:
+                maximumProfit=prices[j] - prices[i]
+            j+=1
+        return maximumProfit
+stock=[7,1,5,3,6,4]
+obj=Solution()
+print(obj.maxProfit(stock))
+
+                
+                
