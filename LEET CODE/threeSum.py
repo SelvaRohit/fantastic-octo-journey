3ed368c5939a375f3df096b0cbf4d5da3efb4dbd
Selva <selvakaruppasamy2018@gmail.com>
2025-09-15 22:46:46 +0530

    modified:LEET CODE/threeSum.py

--- LEET CODE/threeSum.py
+++ LEET CODE/threeSum.py
@@ -4,29 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        # nums=list(set(    nums))
-        nums.sort()
-        out=set()
-        last_element=None
-        for i,curr in enumerate(nums):
-            if last_element == None or last_element!=curr:
-                target=-(curr)
-                hashmap={}
-                for j in range(i+1,len(nums)):
-                    if nums[j] in hashmap:
-                        out.add((curr,nums[hashmap[nums[j]]],nums[j]))
+        for i in range(len(nums)):
+            heap={}
+            target=(0-nums[i])
+            for j in range(len(nums)-i):
+                currentindex=j+i
+                if nums[currentindex] not in heap:
+                    heap[target-nums[currentindex]]=currentindex
+                else:
+                    return i,j,currentindex
+        return None,None,None #if not a any 3 numbers sum is equal to target
+obj=Solution()
+nums=[-1,0,1,2,-1,-4]
+print(obj.threeSum(nums))
 
-                    else:
-                        found=target-nums[j]
-                        hashmap[found]=j
-                last_element=curr
-        out=list(out)
-        output=[]
-        for triplet in out:
-            output.append(list(triplet))
-        return output
-if __name__ == "__main__":
-    obj = Solution()
-    arr=[-1,0,1,2,-1,-4]
-    print(obj.threeSum(arr))
         
