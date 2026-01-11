cfebcfb359e0f578558a0d703519f22e1e3c4176
Selva <selvakaruppasamy2018@gmail.com>
2026-01-11 22:07:36 +0530

    Solved the Search 2D matrix problrm.

--- LEET CODE/Search 2D matrix.py
+++ LEET CODE/Search 2D matrix.py
@@ -10,7 +10,27 @@ class Solution(object):
         rows=len(matrix)
         cols=len(matrix[0])
         start=0
-        end=rows+cols
-        mid=((end-start)//2)+1
-        mid_row_index=mid//cols
-        mid_col_index=mid%cols
+        end=(rows*cols)-1
+        while start <= end:
+            mid=((end+1-start)//2)+start
+            mid_row_index=mid//cols
+            mid_col_index=mid%cols
+            to_check=matrix[mid_row_index][mid_col_index]
+            if  to_check== target:
+                return True
+            elif to_check > target:
+                # start=0
+                end=mid-1
+            else:
+                start=mid+1
+                # end=rows*cols
+            # print(start,end)
+        return False
+
+
+if __name__ == "__main__":
+    obj=Solution()
+    matrix = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+    target = 10
+    out=obj.searchMatrix(matrix=matrix,target=target)
+    print(out)
--- LEET CODE/test.py
+++ LEET CODE/test.py
@@ -1,9 +1,5 @@
-a= '1'
-b='1'
-carry='1'
-i=0
-d= int(a)|int(b)|int(carry)
-print(d)
-carry=((int(a[i])&int(b[i]))|(int(b)&carry)|(carry&int(a)))
-print(carry)
-
+[
+    [1,3,5,7],
+    [10,11,16,20],
+    [23,30,34,60]
+ ]
